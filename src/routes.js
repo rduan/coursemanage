7354d3f7f9513db9275f0d37a0a55c06cfe26a47
Richard Duan <richard@predictivehire.com>
2017-11-04 12:44:55 +1100

    redux

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/homePage';
 import AboutPage from './components/about/aboutPage';
+import CoursePage from './components/course/coursesPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}></IndexRoute>
+    <Route path="courses" component={CoursePage}></Route>
     <Route path="about" component={AboutPage}></Route>
   </Route>
 );
