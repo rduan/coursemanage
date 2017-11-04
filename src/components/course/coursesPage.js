1a569dfe0ffaab6d9fdd661d6b894220187d2afa
Richard Duan <richard@predictivehire.com>
2017-11-05 08:06:02 +1100

    redux

--- src/components/course/coursesPage.js
+++ src/components/course/coursesPage.js
@@ -1,5 +1,8 @@
 import React, {PropTypes} from 'react';
 import {Link, IndexLink} from 'react-router';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions'; 
+import {bindActionCreators} from 'redux';
 
 class CoursePage extends React.Component {
   constructor(props, context) {
@@ -19,13 +22,21 @@ class CoursePage extends React.Component {
   }
 
   onClickSave(){
-    alert(`${this.state.course.title}`);
+    // if no mapDispatchToProps, connect will auto inject the dispatch function
+    // this.props.dispatch(courseActions.createCourse(this.state.course));
+
+    //defined function in mapDispatchToProps,
+    this.props.createCourse(this.state.course);
   }
 
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div> ;
+  }
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add course</h2>
         <input
           type="text"
@@ -42,4 +53,32 @@ class CoursePage extends React.Component {
 
 }
 
-export default CoursePage;
+// dispatch is auto injected by connect if there is no mapDispatchToProps fefined. no createCourse then
+//if defined mapDispatchToProps, will has createCourse, no dispatch 
+CoursePage.propTypes = {
+  //dispatch: React.PropTypes.func.isRequired,
+  courses: React.PropTypes.array.isRequired,
+  createCourse: React.PropTypes.func.isRequired
+}
+
+function mapStateToProps(state,ownProps) {
+  return {
+    courses: state.courses
+  };
+}
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    //when not using bindActionCreators
+    // createCourse: course=> dispatch(courseActions.createCourse(course))
+
+    //using bindActionCreators to map all actions in courseActions , 
+    // actions: bindActionCreators(courseActions, dispatch),
+
+    //using bindActionCreators to map specific actions
+    createCourse: bindActionCreators(courseActions.createCourse, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursePage);
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,7 +1,9 @@
 export default function courseReducer(state=[], action) {
   switch(action.type) {
     case 'CREATE_COURSE': 
-      return state;
+      return [...state,
+        Object.assign({},action.course)
+      ];
 
     default:
       return state;
