ac7e99a92aae045ca3ad3f83e44be274982c9b7c
Richard Duan <richard@predictivehire.com>
2017-11-07 16:49:24 +1100

    clear combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,15 @@
 import {combineReducers} from 'redux';
-import courses from './courseReducer';
+import coursesReducer from './courseReducer';
 import authors from './authorReducer';
 
+
+/**
+ *  can not use coursesReducer, because combineReducers will return a single state object
+ *  be carefule
+ */
+
 const rootReducer = combineReducers({
-  courses,
+  courses: coursesReducer,
   authors
 });
 
